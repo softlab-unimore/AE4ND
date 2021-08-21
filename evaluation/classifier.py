03be7682f86d9e048fc212b28f43140fa201c58a
delbu <FrancescoDelBuono>
2021-08-21 11:10:41 +0200

    increase classifier test

--- evaluation/classifier.py
+++ evaluation/classifier.py
@@ -60,12 +60,9 @@ def main():
     params = get_argument()
     all_state_folder = params['all_state_folder']
     features_list = params['features_list']
-    model_type = params['model_type']
     resample_rate = 6400
 
-    kernel = 80
     stride = 1
-
     epochs = 500
 
     transform_type = 'minmax'
@@ -73,81 +70,91 @@ def main():
     save_result = True
     output_dir = './results'
 
-    skip_list = [0]
-    train_list = [1]
-    ds_train_list = []
-    y_train_list = []
-    ds_test_list = []
-    y_test_list = []
+    for train_id in [1, 2, 3, 4]:
+        skip_list = [0]
+        train_list = [train_id]
+        ds_train_list = []
+        y_train_list = []
+        ds_test_list = []
+        y_test_list = []
 
-    # Read train and test files
-    print('Read all datasets')
-    for state_id, folder in enumerate(all_state_folder):
-        print('\nRead state: ', os.path.basename(folder))
-        files = get_files(folder, ext='lvm')
-        for i, filename in enumerate(files):
-            if i in skip_list:
-                print('Skip:               {}'.format(filename))
-                continue
+        # Read train and test files
+        print('Read all datasets')
+        for state_id, folder in enumerate(all_state_folder):
+            print('\nRead state: ', os.path.basename(folder))
+            files = get_files(folder, ext='lvm')
 
-            ds = read_ds_lvm(filename, get_header=False)
-            ds = ds[features_list]
-            ds = resample(ds, resample_rate)
+            selected_train_id = [x for x in range(len(files)) if x in train_list]
+            if not len(selected_train_id):
+                selected_train_id = [1]
 
-            if i in train_list:
-                print('Train state {} file: {}'.format(state_id, filename))
-                ds_train_list.append(ds)
-                y_train_list.append(state_id)
-            else:
-                print('Test state {} file:  {}'.format(state_id, filename))
-                ds_test_list.append(ds)
-                y_test_list.append(state_id)
+            for i, filename in enumerate(files):
+                if i in skip_list:
+                    print('Skip:               {}'.format(filename))
+                    continue
 
-    # Apply transform
-    transformer = None
-    if transform_type:
-        print('Apply transform: ', transform_type)
-        x_train_list, transformer = transform_data(ds_train_list, transform_type)
-        x_test_list = [apply_transform(ds, transformer) for ds in ds_test_list]
+                # ds = None
+                ds = read_ds_lvm(filename, get_header=False)
+                ds = ds[features_list]
+                ds = resample(ds, resample_rate)
 
-    else:
-        print('No transform selected')
-        x_train_list = ds_train_list
-        x_test_list = ds_test_list
+                if i in selected_train_id:
+                    print('Train state {} file: {}'.format(state_id, filename))
+                    ds_train_list.append(ds)
+                    y_train_list.append(state_id)
+                else:
+                    print('Test state {} file:  {}'.format(state_id, filename))
+                    ds_test_list.append(ds)
+                    y_test_list.append(state_id)
 
-    # Create train and test matrix set
-    x_train, y_train = prepare_data(x_train_list, labels=y_train_list, kernel=kernel, stride=stride)
-    x_test, y_test = prepare_data(x_test_list, labels=y_test_list, kernel=kernel, stride=stride)
+        # Apply transform
+        transformer = None
+        if transform_type:
+            print('Apply transform: ', transform_type)
+            x_train_list, transformer = transform_data(ds_train_list, transform_type)
+            x_test_list = [apply_transform(ds, transformer) for ds in ds_test_list]
 
-    print('Train size:       ', x_train.shape)
-    print('Train label size: ', y_train.shape)
-    print('Test size:        ', x_test.shape)
-    print('Test label size:  ', y_test.shape)
+        else:
+            print('No transform selected')
+            x_train_list = ds_train_list
+            x_test_list = ds_test_list
 
-    order = np.random.permutation(len(x_train))
-    x_new = x_train[order]
-    y_new = y_train[order]
+        for kernel in [40, 80, 120, 200]:
 
-    for model_type in ['classifier', 'linear', 'cnn', 'deep', 'lstm', 'bilstm']:
-        # Model initialization
-        print("Model initialization: {}".format(model_type))
-        model = get_model(model_type)
+            # Create train and test matrix set
+            x_train, y_train = prepare_data(x_train_list, labels=y_train_list, kernel=kernel, stride=stride)
+            x_test, y_test = prepare_data(x_test_list, labels=y_test_list, kernel=kernel, stride=stride)
 
-        # Training
-        print("Training...")
-        model.fit(x=x_new, y=y_new, epochs=epochs, verbose=2)
+            print('Train size:       ', x_train.shape)
+            print('Train label size: ', y_train.shape)
+            print('Test size:        ', x_test.shape)
+            print('Test label size:  ', y_test.shape)
 
-        y_pred = model.predict(x_test, classifier=True)
+            order = np.random.permutation(len(x_train))
+            x_new = x_train[order]
+            y_new = y_train[order]
 
-        print(classification_report(y_test, y_pred))
-        ds_res = pd.DataFrame(classification_report(y_test, y_pred, output_dict=True))
+            for model_type in ['classifier', 'linear', 'cnn', 'deep', 'lstm', 'bilstm']:
 
-        if save_result:
-            if not os.path.isdir(output_dir):
-                os.makedirs(output_dir, exist_ok=True)
+                # Model initialization
+                print("Model initialization: {}".format(model_type))
+                model = get_model(model_type)
 
-            filename = os.path.join(output_dir, 'results_accuracy_' + model_type + '.csv')
-            ds_res.to_csv(filename, index=True)
+                # Training
+                print("Training...")
+                model.fit(x=x_new, y=y_new, epochs=epochs, verbose=2)
+
+                y_pred = model.predict(x_test, classifier=True)
+
+                print(classification_report(y_test, y_pred))
+                ds_res = pd.DataFrame(classification_report(y_test, y_pred, output_dict=True))
+
+                if save_result:
+                    if not os.path.isdir(output_dir):
+                        os.makedirs(output_dir, exist_ok=True)
+
+                    filename = os.path.join(output_dir, 'results_{}_accuracy_{}_{}.csv'.format(train_id, model_type, kernel))
+                    ds_res.to_csv(filename, index=True)
 
 
 if __name__ == '__main__':
